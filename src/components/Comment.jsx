10a1933cee136dc974e30c1669054c689cb62707
Samyusuf101 <samyusuf101@gmail.com>
2022-10-31 06:37:32 -0700

    created comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+function Comment() {
+  return <div>Comment</div>;
+}
+
+export default Comment;
---
+++ src/components/Comments.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+function Comments() {
+  return <div>Comments</div>;
+}
+
+export default Comments;
